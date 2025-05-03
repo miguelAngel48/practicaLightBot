e10b82865d0b9efc70d8839ce9e1f3d7b12ea187
miguelAngel48 <miguelangelponcesnachez@yahoo.es>
2025-05-03 18:19:15 +0200

    Prueba de commits y mover con checkout

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    //Prueba de texto en archivo de java para practicar con los commits desde terminal 
 }
